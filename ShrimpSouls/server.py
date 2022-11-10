c20fbdada0a8ec73e819dc6d84db9c0c10e73e73
DistractionCrab <6255349+DistractionCrab@users.noreply.github.com>
2022-11-10 04:46:43 -0500

    Update server.py

--- ShrimpSouls/server.py
+++ ShrimpSouls/server.py
@@ -117,6 +117,7 @@ class Server:
 				if i == 0:
 					self.__game.campaign.clear_npcs()
 					self.__game.campaign.clear_players()
+					print("clearing.")
 			else:
 				self.__i_time = time.time()
 				if msg['msg'] == "connect":
